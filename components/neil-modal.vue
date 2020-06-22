3abc0ac7ae0a2b33b3d58ba420805ca7202ddd6f
臻 <408869403@qq.com>
2020-06-23 01:16:12 +0800

    fix: 小程序输入框隐藏，placeholder显示的问题

--- components/neil-modal.vue
+++ components/neil-modal.vue
@@ -1,7 +1,7 @@
 <template>
     <view class="neil-modal" @touchmove.stop.prevent="bindTouchmove" :class="{'neil-modal--show':isOpen}">
         <view class="neil-modal__mask" @click="clickMask"></view>
-        <view class="neil-modal__container">
+        <view class="neil-modal__container" v-show="isOpen">
             <view class="neil-modal__header" v-if="title.length > 0">{{title}}</view>
             <view class="neil-modal__content" :class="content ? 'neil-modal--padding' : ''" :style="{textAlign:align}">
                 <template v-if="content">
